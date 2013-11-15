f449eabff4e5c7919b65b086e86009435c96d627
alkchen <alkchen@gmail.com>
2013-11-14 23:22:41 -0800

    clean up and create products and variants in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Rails.logger.info "Cleaning data"
+# truncating all tables
+ActiveRecord::Base.establish_connection
+ActiveRecord::Base.transaction do
+  ActiveRecord::Base.connection.tables.each do |table|
+    next if %w{schema_migrations}.include?(table)
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} CASCADE;")
+  end
+end
 
-puts "START SEEDING"
+require 'factory_girl_rails'
 
+Rails.logger.info "START SEEDING"
 Country.create_all
 State.create_all
 Role.create_all
@@ -23,3 +26,9 @@ ReturnReason.create_all
 ReturnCondition.create_all
 ReferralBonus.create_all
 ReferralProgram.create_all
+
+Rails.logger.info "SEEDING PRODUCTS"
+FactoryGirl.create_list(:product, 10)
+FactoryGirl.create_list(:variant, 10)
+
+Rails.logger.info "SEEDING ORDERS"
