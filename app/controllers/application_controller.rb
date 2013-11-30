0a68b2ed34717eecbf73ea241a635e6b7fe3ccf2
Alex Chen <alexchen@alexs-air.gateway.pace.com>
2013-11-30 01:05:37 -0800

    get rid of cookie[:cart_id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,36 +67,16 @@ class ApplicationController < ActionController::Base
     cookies[:insecure] = false    
   end
 
-  # def current_user_id
-  #   return @current_user_id if defined?(@current_user_id)
-  #   @current_user_id = current_user_session && current_user_session.record && current_user_session.record.id
-  #end
-
-
   def session_cart
     return @session_cart if defined?(@session_cart)
-    session_cart!
-  end
-
-  # use this method if you want to force a SQL query to get the cart.
-  def session_cart!
-    return unless current_user
-    if cookies[:cart_id]
-      @session_cart = Cart.includes(:shopping_cart_items).find_by_id(cookies[:cart_id])
-      unless @session_cart
-        @session_cart = Cart.create(:user_id => current_user.id)
-        cookies[:cart_id] = @session_cart.id
-      end
-    elsif current_user && current_user.current_cart
+    if current_user && current_user.current_cart
       @session_cart = current_user.current_cart
-      cookies[:cart_id] = @session_cart.id
     else
-      @session_cart = Cart.create
-      cookies[:cart_id] = @session_cart.id
+      @session_cart = Cart.create    
     end
     @session_cart
   end
-  
+
   ###  Authlogic helper methods
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
