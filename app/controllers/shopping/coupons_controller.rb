b7e277c46442fb455ad93225b5799be8a1a0ca94
Alex Chen <alexchen@alexs-air.gateway.pace.com>
2013-11-29 21:29:48 -0800

    stylging admin offers

--- app/controllers/shopping/coupons_controller.rb
+++ app/controllers/shopping/coupons_controller.rb
@@ -5,8 +5,8 @@ class Shopping::CouponsController < Shopping::BaseController
 
   def create
     @coupon = Coupon.find_by_code(params[:coupon][:code])
-
-    if @coupon && @coupon.eligible?(session_order) && update_order_coupon_id(@coupon.id)
+    if @coupon && @coupon.eligible?(session_order) && 
+      session_order.update_attributes( :coupon_id => @coupon.id )
       flash[:notice] = "Successfully added coupon code #{@coupon.code}."
       redirect_to next_form_url(session_order)
     else
@@ -20,11 +20,5 @@ class Shopping::CouponsController < Shopping::BaseController
 
   def form_info
     @coupon = Coupon.new
-  end
-
-  def update_order_coupon_id(id)
-    session_order.update_attributes(
-                          :coupon_id => id
-                                    )
   end
 end
