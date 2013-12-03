37d28581607680116f479fd759a76e46584d91d4
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-03 01:52:07 -0800

    fix inventory spec

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,45 +1,36 @@
-# == Schema Information
-#
-# Table name: inventories
-#
-#  id                          :integer          not null, primary key
-#  count_on_hand               :integer          default(0)
-#  count_pending_to_customer   :integer          default(0)
-#  count_pending_from_supplier :integer          default(0)
-#
-
 require 'spec_helper'
 
 describe Inventory do
+  before(:each) do 
+    @inventory = FactoryGirl.build(:inventory)
+  end
   context 'validation working' do
     it 'should not save inventory below out_of_stock limit' do
-      inventory = FactoryGirl.build(:inventory,
-      :count_on_hand       =>      10,
-      :count_pending_to_customer => 11)
-      inventory.valid?.should == false
+      @inventory.assign_attributes(
+        :count_on_hand => 10,
+        :count_pending_to_customer => 11)
+      @inventory.valid?.should == false
     end
 
     it 'should not save inventory below out_of_stock limit' do
-      inventory = FactoryGirl.build(:inventory,
-                                    :count_on_hand => 10,
-                                    :count_pending_to_customer => 11 - Variant::OUT_OF_STOCK_QTY)
-      inventory.valid?.should == true
-      inventory.save.should be_true
+      @inventory.assign_attributes(
+        :count_on_hand => 10,
+        :count_pending_to_customer => 9)
+      @inventory.valid?.should == true
+      @inventory.save.should be_true
     end
 
     it 'should not save inventory below out_of_stock limit' do
-      inventory = FactoryGirl.build(:inventory,
-      :count_on_hand       =>      100 ,
-      :count_pending_to_customer => 101 - Variant::LOW_STOCK_QTY)
-      inventory.valid?.should == true
+      @inventory.assign_attributes(
+        :count_on_hand => 100 ,
+        :count_pending_to_customer => 101 - Variant::LOW_STOCK_QTY)
+      @inventory.valid?.should == true
     end
     it 'should not save inventory below out_of_stock limit' do
-      inventory = FactoryGirl.build(:inventory,
-      :count_on_hand       =>      100 + Variant::LOW_STOCK_QTY,
-      :count_pending_to_customer => 0)
-      inventory.valid?.should == true
+      @inventory.assign_attributes(
+        :count_on_hand => 100 + Variant::LOW_STOCK_QTY,
+        :count_pending_to_customer => 0)
+      @inventory.valid?.should == true
     end
-
   end
-
 end
