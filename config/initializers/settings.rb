1ed417354121c9d46bddee0b4d0097001ddd16a0
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-24 00:54:30 -0800

    remove redundant text

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -5,25 +5,5 @@
 require 'yaml'
 
 SETTINGS = {
-  :admin_grid_rows  => 25,
   :use_foreign_keys => false
 }
-
-#APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "config.yml"))[Rails.env]
-
-=begin
-### These options are stored in each of the environment files
-
-#Paperclip.options[:image_magick_path] = '/opt/local/bin'
-if RAILS_ENV == "development"
-
-  Paperclip.options[:command_path] = "DYLD_LIBRARY_PATH='/Users/davidhenner/ImageMagick-6.6.3/lib' /Users/davidhenner/ImageMagick-6.6.3/bin"
-
-  #Paperclip.options[:command_path] = '/Users/davidhenner/ImageMagick-6.6.3/bin'
-  #Paperclip.options[:command_path] = '/usr/local/bin'
-  ###  UNCOMMENT IF USING MACPORTS
-  #Paperclip.options[:command_path] = '/opt/local/bin'
-else
-  Paperclip.options[:command_path] = '/usr/bin'
-end
-=end
