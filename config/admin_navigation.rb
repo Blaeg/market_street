a6ad883a7d921b1bc41289115f190dfe645af60b
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-24 22:18:45 -0800

    nav by config, minor adjustment

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -3,16 +3,21 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav'
     primary.auto_highlight = true
+    
+    primary.item :user_tab ,"Customer", '#' do |user|
+      user.item :users ,"Users", admin_customer_users_path
+      user.item :referrals ,"Referrals", admin_customer_referrals_path
+    end
+
     primary.item :catalog_tab, 'Catalog', '#' do |catalog|
       catalog.item :products, 'Products', admin_catalog_products_path
       catalog.item :properties, 'Properties', admin_catalog_properties_path 
       catalog.item :product_types, 'Product Types', admin_catalog_product_types_path 
       catalog.item :prototypes, 'Prototypes', admin_catalog_prototypes_path
-      catalog.item :image_groups, 'Image Groups', admin_catalog_image_groups_path
-      catalog.item :brands, 'Brands', admin_catalog_brands_path
+      catalog.item :image_groups, 'Image Groups', admin_catalog_image_groups_path      
     end
 
-    primary.item :inventory_tab, 'Inventory', admin_inventory_overviews_path do |inventory|
+    primary.item :inventory_tab, 'Inventory', '#' do |inventory|
       inventory.item :overview, 'Overviews', admin_inventory_overviews_path
       inventory.item :suppliers, 'Suppliers', admin_inventory_suppliers_path
       inventory.item :receive_po, 'Receive Purchase Order', admin_inventory_receivings_path
@@ -20,31 +25,26 @@ SimpleNavigation::Configuration.run do |navigation|
       inventory.item :adjusments, 'Adjustments', admin_inventory_adjustments_path 
     end
 
-    primary.item :offer_tab ,"Offer", admin_offer_coupons_path do |offer|        
+    primary.item :offer_tab ,"Offer", '#' do |offer|        
       offer.item :coupons, 'Coupons', admin_offer_coupons_path
       offer.item :deals,'Deals', admin_offer_deals_path
       offer.item :sales, 'Sales', admin_offer_sales_path
     end
 
-    primary.item :orders_tab, 'Order', admin_history_orders_path do |order|
+    primary.item :orders_tab, 'Order', '#' do |order|
       order.item :orders, 'Orders', admin_history_orders_path
       order.item :shipments, 'Shipments', admin_fulfillment_shipments_path
       order.item :invoices, 'Invoices', admin_document_invoices_path
       order.item :fulfillments, 'Fulfillments', admin_fulfillment_orders_path
     end
 
-    primary.item :user_tab ,"User", admin_customer_users_path do |user|
-      user.item :users ,"Users", admin_customer_users_path
-      user.item :referrals ,"Referrals", admin_customer_referrals_path
-    end
-
     if current_user.super_admin?
-      primary.item :config_tab ,"Config", admin_config_tax_rates_path do |config|        
-        config.item :countries,'Countries', admin_config_countries_path
+      primary.item :config_tab ,"Config", '#' do |config|        
         config.item :shipping_zones, 'Shipping Zones', admin_config_shipping_zones_path
         config.item :shipping_methods, 'Shipping Methods', admin_config_shipping_methods_path
         config.item :shipping_rates, 'Shipping Rates', admin_config_shipping_rates_path 
         config.item :tax_rates, 'Tax Rates', admin_config_tax_rates_path
+        config.item :countries,'Countries', admin_config_countries_path        
       end
     end   
   end
