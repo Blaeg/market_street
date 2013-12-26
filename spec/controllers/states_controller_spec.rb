e1cc993c2d822ab5e0012b6e47ce0adf1b37d76e
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-25 23:34:05 -0800

    expect success

--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -5,6 +5,6 @@ describe StatesController do
   it "index action renders index template" do
     request.env["HTTP_ACCEPT"] = "application/json"
     get :index, :country_id => 2
-    assert_response :success
+    expect(response).to be_success
   end
 end
