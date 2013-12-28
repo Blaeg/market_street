125e7aa08b4272b4ac5ba445f0d9ad747efeb188
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-28 01:38:58 -0800

    fix add to cart js bug

--- app/themes/default/assets/javascripts/shopping/cart.js
+++ app/themes/default/assets/javascripts/shopping/cart.js
@@ -23,17 +23,16 @@ if (typeof MarketStreet.Cart.newForm == "undefined") {
       )
       // This code is to change the color of the selected and non-selected variants
       jQuery('.variant_select').click( function() {
-
           jQuery('.variant_properties').each( function(index, obj) {
             jQuery(obj).removeClass('selected');
           });
 
-          var propId = '#variant_properties_' + $(this).data("variant_id");
+          var propId = '#variant_properties_' + $(this).data("variant_id");          
         $( propId ).addClass('selected');
           jQuery('#cart_item_variant_id').val($(this).data("variant_id"));
           jQuery(".variant_select").removeClass('selected_variant');
           jQuery(this).addClass('selected_variant');
-          //jQuery('#submit_add_to_cart').removeClass('add-to-cart').addClass('ready-to-add-to-cart');
+          jQuery('#submit_add_to_cart').removeClass('add-to-cart').addClass('ready-to-add-to-cart');
         }
       );
     }
