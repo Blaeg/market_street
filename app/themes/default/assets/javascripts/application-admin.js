78dee2692297bad347a0a962f72db800f997c056
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-27 19:14:59 -0800

    remove jquery orbit, revel and tooltips

--- app/themes/default/assets/javascripts/application-admin.js
+++ app/themes/default/assets/javascripts/application-admin.js
@@ -1,5 +1,3 @@
 //= require admin/app
-//= require admin/jquery.customforms
 //= require admin/jquery.placeholder.min
-//= require admin/jquery.reveal
-//= require admin/jquery.tooltips
+//= require admin/jquery.customforms
