1fb7c256512e792c6fc8ea7af410827fcdbc7ca6
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-31 01:34:44 -0800

    cart summary

--- app/models/cart/calculator.rb
+++ app/models/cart/calculator.rb
@@ -2,6 +2,18 @@ module Cart::Calculator
   def sub_total
     cart_items.map(&:total).sum
   end
+  
+  def shipping
+  	0.0
+  end
+
+  def tax 
+  	0.0
+  end
+  
+  def total
+  	cart_items.map(&:total).sum
+  end
 
   def number_of_cart_items
     cart_items.map(&:quantity).sum
