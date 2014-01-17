5324c52a0af5312a4af5276a20401787f1f74a2c
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2014-01-17 03:04:42 -0800

    delete unnecessary files and tests

--- app/models/order_item/calculator.rb
+++ app/models/order_item/calculator.rb
@@ -1,40 +1,10 @@
 module OrderItem::Calculator
   def calculate_order
-    set_beginning_values
-    #order.calculate_totals
+    set_beginning_values    
   end
 
   def adjusted_price(coupon = nil)
-    ## coupon credit is calculated at the order level but because taxes we need to apply it now
     coupon_credit = coupon ? coupon.value([sale_price(order.completed_at)], order) : 0.0
     self.price - coupon_credit
-  end
-
-  # def sale_price(at)
-  #   sale = sale_at(at)
-  #   sale ? ( (1.0 - sale.percent_off) * self.price ).round_at(2) : self.price
-  # end
-
-  # def item_total(coupon = nil)
-  #   self.total_amount ||= calculate_total(coupon)
-  # end
-
-  # def calculate_total(coupon = nil)
-  #   self.total = (adjusted_price(coupon)).round_at(2)
-  # end
-
-  def tax_charge
-    tax_percentage = tax_rate.try(:tax_percentage) ? tax_rate.tax_percentage : 0.0
-    adjusted_price * tax_percentage / 100.0
-  end
-
-  def amount_of_charge_is_vat
-    vat_percentage = tax_rate.try(:vat_percentage) ? tax_rate.vat_percentage : 0.0
-    (adjusted_price * (vat_percentage / (100.0 + vat_percentage))).round_at(2)
-  end
-
-  def amount_of_charge_without_vat
-    vat_percentage = tax_rate.try(:vat_percentage) ? tax_rate.vat_percentage : 0.0
-    (100.0 * adjusted_price / (100.0 + vat_percentage)).round_at(2)
   end  
 end
--- lib/printing/main_printer.rb
+++ lib/printing/main_printer.rb
@@ -35,32 +35,5 @@ module MainPrinter
       arguements[:size]  = 13
     end
     pdf.draw_text display_string, arguements
-  end
-  
-  ## method: checker_form(pdf)
-  ## This is a method to see the x-y locations throughout your document
-  # This is a good method for developing a new form
-  # input:  pdf
-  # returns: pdf with x-y grid
-
-  def checker_form(pdf)
-    add_num = 5
-    0.upto(65) { |i| pdf.draw_text "#{(i*10+add_num).to_s},#{(i*15).to_s}",  :at => [(i*10+add_num),(i*15)], :size => 10 }
-    0.upto(65) { |i| pdf.line [(i*10+add_num),0], [(i*10+add_num),800] }
-    0.upto(75) { |i| pdf.draw_text "#{(i*10 + add_num).to_s}",             :at => [100,(i*10 + add_num)], :size => 10 }
-    0.upto(75) { |i| pdf.line [0,(i*10 + add_num)], [800,(i*10 + add_num)] }
-#
-#    0.upto(60) { |i| pdf.text "#{(i*10).to_s},40",  :at => [(i*10),(i*15)], :size => 8 if i.modulo(5) == 0 }
-#    0.upto(60) { |i| pdf.text "#{(i*10).to_s},755", :at => [(i*10),(i*15)], :size => 8 if i.modulo(5) == 0 }
-#    0.upto(75) { |i| pdf.text "36,#{(i*10).to_s}",  :at => [0,(i*10)],    :size => 8 }
-#    0.upto(75) { |i| pdf.text "590,#{(i*10).to_s}", :at => [100,(i*10)],    :size => 8 }
-    pdf.stroke()
-  end
-
-  def checker_background(pdf, png_background = nil)
-      pdf.bounding_box([10,730], :width => 530) do
-          pdf.image png_background, :width => 519 if png_background
-          checker_form(pdf)
-      end
-  end # end of print_form method
+  end  
 end
